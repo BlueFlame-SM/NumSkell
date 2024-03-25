fef034ec4e326c64ee2cd12d4a0c38191cfe7584
BenjaminMeijs <benjamin.meijs@gmail.com>
2024-03-25 15:13:42 +0100

    feat: Added index for matrix - 2

--- src/Data/WrappedHmatrixMatrix.hs
+++ src/Data/WrappedHmatrixMatrix.hs
@@ -1,6 +1,9 @@
-{-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
-
+{-# LANGUAGE KindSignatures #-}
+{-# LANGUAGE RankNTypes #-}
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeOperators #-}
 {-
  - This wrapper uses the non-static interface for hmatrix, but there is an
  - (experimental?) static interfaces that has "statically checked dimensions",
@@ -13,23 +16,56 @@
 
 module Data.WrappedHmatrixMatrix where
 
-import qualified Numeric.LinearAlgebra as H
+import Data.Singletons
+    ( withSingI,
+      Sing,
+      SingI(..),
+      SingKind(fromSing, toSing),
+      SomeSing(SomeSing) )
+import GHC.TypeLits.Singletons
+import Prelude.Singletons
+import Data.Array (Array)
+import qualified Data.Array as A
+
+-- import qualified Numeric.LinearAlgebra as H
 import Prelude hiding ((!!))
 
 type Shape = (Int, Int)
 type Index = (Int, Int)
 
-data Matrix a where
-    Matrix :: (H.Element a, H.Indexable (H.Vector a) a) => {shape :: Shape, hMatrix :: H.Matrix a} -> Matrix a
+-- type Shape = (Natural, Natural)
 
-(!!) :: Matrix a -> Index -> a
-Matrix _ hMatrix !! (x, y) = hMatrix H.! y H.! x
+type Matrix n m a = Array m (Array n a)
 
-(!?) :: Matrix a -> Index -> Maybe a
-m@(Matrix (cols, rows) hMatrix) !? i@(x, y)
-    | x < cols && y < rows = Just $ m !! i
-    | otherwise = Nothing
+-- newtype Matrix (s :: Shape) where
+--     Matrix :: (H.Element a, H.Indexable (H.Vector a) a)
+--                 => H.Matrix a
+--                 -> Matrix s a
+
+-- index :: 
+            -- (KnownNat x, KnownNat y, KnownNat n, KnownNat m,
+            -- (i <= n) ~ 'True, (j <= m) ~ 'True) =>  
+            -- Matrix n m a -> proxy i -> proxy j -> a
+-- index m i j = A.index (A.index m j) i
+
+indexRow :: (KnownNat i, (i <= m) ~ 'True) => 
+            Matrix n m a -> proxy i -> Array n a
+indexRow = A.index 
+
+index :: (KnownNat m', KnownNat n', (m' <= m) ~ 'True, (n' <= n) ~ 'True) =>
+        Matrix n m a -> proxy m' -> proxy n' -> a
+index mat m n = A.index (A.index mat m ) n
+
+
+
+
+-- (!?) :: Matrix a -> Index -> Maybe a
+-- m@(Matrix (cols, rows) hMatrix) !? i@(x, y)
+--     | x < cols && y < rows = Just $ m !! i
+--     | otherwise = Nothing
 
 {- Examples -}
-a = Matrix (2, 3) $ (2 H.>< 3) [1, 2, 3, 4, 5, 6] :: Matrix H.R
-b = Matrix (2, 3) $ (2 H.>< 3) [6, 5, 4, 3, 2, 1] :: Matrix H.C
+exampleA = A.arr3 (A.arr3 1 2 3) (A.arr3 4 5 6) (A.arr3 7 8 9)
+
+exampleIndexRow = (indexRow exampleA (Proxy :: Proxy 1))
+-- b = Matrix (2, 3) $ (2 H.>< 3) [6, 5, 4, 3, 2, 1] :: Matrix H.C
