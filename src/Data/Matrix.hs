b8d8a0907b0deb475324407a73d15c3886042ae0
Christoffer Gram <74768013+toffernator@users.noreply.github.com>
2024-03-28 17:10:36 +0100

    feat: idMatrix

--- src/Data/Matrix.hs
+++ src/Data/Matrix.hs
@@ -12,6 +12,7 @@ module Data.Matrix where
 
 import Data.Array (Array)
 import qualified Data.Array as A
+import Data.List (intercalate)
 import Data.Singletons (
     Sing,
     SingI (..),
@@ -19,25 +20,22 @@ import Data.Singletons (
     SomeSing (SomeSing),
     withSingI,
  )
+import Data.Vector (Vector, (!))
+import qualified Data.Vector as V
 import GHC.TypeLits.Singletons
 import Prelude.Singletons
-import qualified Data.Vector as V
-import Data.Vector (Vector, (!))
-import Data.List (intercalate)
 
 -- import qualified Numeric.LinearAlgebra as H
 import Prelude hiding (replicate, zipWith, (!!))
+import qualified Prelude as P
 
 type Shape = (Int, Int)
 type Index = (Int, Int)
 
 -- type Shape = (Natural, Natural)
 
-newtype Matrix (n :: Natural) (m :: Natural) a = Matrix { toVector :: Vector a }
+newtype Matrix (n :: Natural) (m :: Natural) a = Matrix {toVector :: Vector a}
     deriving (Eq, Ord)
-
-
-
 
 empty :: Matrix 0 0 a
 empty = Matrix V.empty
@@ -45,20 +43,20 @@ empty = Matrix V.empty
 singleton :: a -> Matrix 1 1 a
 singleton x = Matrix $ V.singleton x
 
-index_ :: (KnownNat i, KnownNat j, (j <= m) ~ True, (i <= n) ~ True) =>  Sing n -> Sing m -> Matrix n m a -> Proxy i -> Proxy j -> a
+index_ :: (KnownNat i, KnownNat j, (j <= m) ~ True, (i <= n) ~ True) => Sing n -> Sing m -> Matrix n m a -> Proxy i -> Proxy j -> a
 index_ n m (Matrix v) i j = v ! (i' * m' + j')
   where
     i' = fromIntegral $ natVal i
     j' = fromIntegral $ natVal j
     m' = fromIntegral $ fromSing m
 
-index :: forall n i m j a . (KnownNat i, KnownNat j, (j <= m) ~ True, (i <= n) ~ True, SingI n, SingI m) => Matrix n m a -> Proxy i -> Proxy j -> a
+index :: forall n i m j a. (KnownNat i, KnownNat j, (j <= m) ~ True, (i <= n) ~ True, SingI n, SingI m) => Matrix n m a -> Proxy i -> Proxy j -> a
 index = index_ (sing :: Sing n) (sing :: Sing m)
 
 replicate_ :: Sing n -> Sing m -> a -> Matrix n m a
 replicate_ n m = Matrix . V.replicate (fromIntegral $ fromSing n * fromSing m)
 
-replicate :: forall n m a . (SingI n, SingI m) => a -> Matrix n m a
+replicate :: forall n m a. (SingI n, SingI m) => a -> Matrix n m a
 replicate = replicate_ (sing :: Sing n) (sing :: Sing m)
 
 zipWith :: (SingI n, SingI m) => (a -> b -> c) -> Matrix n m a -> Matrix n m b -> Matrix n m c
@@ -91,21 +89,29 @@ instance (SingI n, SingI m, Semigroup a) => Semigroup (Matrix n m a) where
 instance (SingI n, SingI m, Monoid a) => Monoid (Matrix n m a) where
     mempty = replicate mempty
 
-
 showMatrix_ :: (Show a) => Sing n -> Sing m -> Matrix n m a -> String
-showMatrix_ n m (Matrix v) = "[" ++ intercalate "\n " (map showRow [0..n'-1]) ++ "]"
+showMatrix_ n m (Matrix v) = "[" ++ intercalate "\n " (map showRow [0 .. n' - 1]) ++ "]"
   where
     n' = fromIntegral $ fromSing n
     m' = fromIntegral $ fromSing m
     showRow i = "[" ++ unwords (V.toList $ show <$> V.slice (i * m') m' v) ++ "]"
 
-showMatrix :: forall n m a . (SingI n, SingI m, Show a) => Matrix n m a -> String
+showMatrix :: forall n m a. (SingI n, SingI m, Show a) => Matrix n m a -> String
 showMatrix (Matrix v) = showMatrix_ (sing :: Sing n) (sing :: Sing m) (Matrix v)
 
 instance (SingI n, SingI m, Show a) => Show (Matrix n m a) where
     show = showMatrix
 
-
 exampleA :: Matrix 2 2 Int
 exampleA = Matrix $ V.fromList [1, 2, 3, 4]
+
 -- exampleIndexRow = (indexRow exampleA (Proxy :: Proxy 1))
+
+idMatrix_ :: (Num a) => Sing n -> Sing m -> Matrix n m a
+idMatrix_ n m = Matrix $ V.fromList $ [if i == j then 1 else 0 | i <- [1 .. n'], j <- [1 .. m']]
+  where
+    n' = fromIntegral $ fromSing n
+    m' = fromIntegral $ fromSing m
+
+idMatrix :: forall n m a. (Num a, SingI n, SingI m) => Matrix n m a
+idMatrix = idMatrix_ (sing :: Sing n) (sing :: Sing m)
