4afb21fd0e77dfecd1f905adda034f971f14a795
BenjaminMeijs <benjamin.meijs@gmail.com>
2024-04-11 14:35:14 +0200

    add beginning of matrix tests

--- test/Main.hs
+++ test/Main.hs
@@ -6,7 +6,8 @@ import Data.Array
 import Data.Maybe (fromJust)
 import GHC.TypeNats
 
-import ArrayTest (arrayProps)
+import ArrayTest (arrayProps, arrayUnitTests)
+import MatrixTest (matrixProps, matrixUnitTests)
 import Test.Tasty (TestTree, defaultMain, localOption, testGroup)
 import Test.Tasty.QuickCheck as QC
 
@@ -14,7 +15,10 @@ main :: IO ()
 main = defaultMain tests
 
 tests :: TestTree
-tests = testGroup "Tests" [properties]
+tests = testGroup "Tests" [properties, unitTests]
 
 properties :: TestTree
-properties = testGroup "Properties" [arrayProps]
+properties = testGroup "Properties" [arrayProps, matrixProps]
+
+unitTests :: TestTree
+unitTests = testGroup "Unit tests" [arrayUnitTests, matrixUnitTests]
